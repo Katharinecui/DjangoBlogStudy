9176ace46a8e9736b84362884f58d3e232e02b03
Katharine <clxycwn@163.com>
2022-08-05 16:17:12 +0800

    注册后台admin模型及定制后台

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author'] # 定制后台文章列表页面
+    fields = ['title', 'body', 'excerpt', 'category', 'tags'] # Post表单展示字段
+
+    def save_model(self, request, obj, form, change): # 自动填充作者
+        obj.author = request.user
+        super().save_model(request, obj, form, change)
+
+# admin后台注册模型
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User # 导入文章作者
+from django.utils import timezone # 导入时间模块
 
 # Create your models here.
 
@@ -8,6 +9,10 @@ from django.contrib.auth.models import User # 导入文章作者
 class Category(models.Model): 
     name = models.CharField(max_length=100) # 分类名为name
 
+    class Meta: # 后台汉化
+        verbose_name = '分类'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.name
 
@@ -15,22 +20,34 @@ class Category(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta: # 后台汉化
+        verbose_name = '标签'
+        verbose_name_plural = verbose_name
+
     def __str__(self):
         return self.name
 
 # 文章
 class Post(models.Model):
-    title = models.CharField(max_length=70) # 文章标题
-    body = models.TextField() # 文章正文
-    created_time = models.DateTimeField() # 文章的创建时间
-    modified_time = models.DateTimeField() # 文章的最后一次修改时间
-    excerpt = models.CharField(max_length=200, blank=True) # 文章摘要
+    title = models.CharField('标题', max_length=70) # 文章标题
+    body = models.TextField('正文') # 文章正文
+    created_time = models.DateTimeField('创建时间', default=timezone.now) # 文章的创建时间
+    modified_time = models.DateTimeField('修改时间') # 文章的最后一次修改时间
+    excerpt = models.CharField('摘要', max_length=200, blank=True) # 文章摘要
 
     # 关联文章对应的数据库表和分类、标签对应的数据库表
-    category = models.ForeignKey(Category, on_delete=models.CASCADE) # 一对多对应关系
-    tags = models.ManyToManyField(Tag, blank=True) # 多对多关系
+    category = models.ForeignKey(Category, verbose_name='分类', on_delete=models.CASCADE) # 一对多对应关系
+    tags = models.ManyToManyField(Tag, verbose_name='标签', blank=True) # 多对多关系
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # 一对多
+    author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE) # 一对多
+
+    class Meta: # 后台汉化
+        verbose_name = '文章'
+        verbose_name_plural = verbose_name
+
+    def save(self, *args, **kwargs): # 自动填充修改时间
+        self.modified_time = timezone.now()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
