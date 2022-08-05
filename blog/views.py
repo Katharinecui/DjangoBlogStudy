8d7bd29ae7069407573db7e87025bc9f3240fd73
Katharine <clxycwn@163.com>
2022-08-05 10:28:29 +0800

    使用模板系统

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 # 视图函数
 
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页！")
+    return render(request, 'blog/index.html', context={ 
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页'
+    })
